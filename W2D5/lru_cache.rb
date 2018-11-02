01539cbf65a8e3b080265dc531880562de93cee1
TsoIsTheWayToGo <ericgordontso@gmail.com>
2018-11-02 08:05:58 -0700

    Add method to lru cache class

---
+++ W2D5/lru_cache.rb
@@ -0,0 +1,31 @@
+class LRUCache
+  def initialize(size)
+    @size = size
+    @cache = []
+  end
+
+  def count
+    @cache.count# returns number of elements currently in cache
+  end
+
+  def add(el)
+  if @cache.include?(el)
+    @cache.delete(el)
+    @cache << el
+  elsif count >= @size
+    @cache.shift
+    @cache << el
+  else
+    @cache << el
+  end  # adds element to cache according to LRU principle
+  end
+
+  def show
+    p @cache
+    nil
+  end
+
+  private
+  # helper methods go here!
+
+end
