5d5ad3edefd0757e3c47c8b256b36aa65c671bea
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-01-25 08:55:38 -0800

    Fill in missing method

--- W3d2/plays.rb
+++ W3d2/plays.rb
@@ -35,9 +35,7 @@ def self.find_by_playwright(name)
     playwright = Playwright.find_by_name(name)
 
     plays = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
-      SELECT
-        *
-      FROM
+
         plays
       WHERE
         playwright_id = ?
